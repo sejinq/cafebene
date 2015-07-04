ce9f31fc109fb3acdd4eb97f13775c5fde771dee
Kim Hyeonwoo <1reds@naver.com>
2015-07-04 13:35:55 +0900

    Splash만 완성해서 올림. 카카오톡 연동은 자꾸 오류남... ㅠㅠ

---
+++ app/src/main/java/cosmantic/cosmantic_khw/SplashActivity.java
@@ -0,0 +1,29 @@
+package cosmantic.cosmantic_khw;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/** 스플래시 (로딩화면) 액티비티 */
+public class SplashActivity extends AppCompatActivity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+		try
+		{
+			// 2초간 대기하고
+			Thread.sleep(2000);
+		}
+		catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+
+		// 메인 액티비티로 넘어간다.
+		startActivity(new Intent(this, MainActivity.class));
+		finish();
+	}
+}
