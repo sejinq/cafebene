6afcffea5532378a532ed2427d7dd34793c0aadf
Kim Hyeonwoo <1reds@naver.com>
2015-07-25 20:44:29 +0900

    액션바 삽입 및 RecommendDetailActivity에 xml 부착

--- app/src/main/java/cosmantic/cosmantic_khw/RecommendDetailActivity.java
+++ app/src/main/java/cosmantic/cosmantic_khw/RecommendDetailActivity.java
@@ -2,14 +2,23 @@ package cosmantic.cosmantic_khw;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.Window;
 
 public class RecommendDetailActivity extends Activity {
     @Override
-    protected void onCreate(Bundle savedInstance){
-        super.onCreate(savedInstance);
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_ACTION_BAR);
+        setContentView(R.layout.activity_recommenddetail);
     }
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actionbar, menu);
+
+        return true;
     }
 }
