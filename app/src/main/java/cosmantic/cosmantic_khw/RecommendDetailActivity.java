5f106c2b6a4db2a4fba406f3d77babc5f33a16a4
Kim Hyeonwoo <1reds@naver.com>
2015-07-29 08:31:23 +0900

    액션바 완성. 폰트가 문제 있어서 해결 못함.

--- app/src/main/java/cosmantic/cosmantic_khw/RecommendDetailActivity.java
+++ app/src/main/java/cosmantic/cosmantic_khw/RecommendDetailActivity.java
@@ -1,24 +1,41 @@
 package cosmantic.cosmantic_khw;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.Window;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RecommendDetailActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_ACTION_BAR);
         setContentView(R.layout.activity_recommenddetail);
+
+        ActionBar mActionBar = getActionBar();
+        mActionBar.setDisplayShowHomeEnabled(false);
+        mActionBar.setDisplayShowTitleEnabled(false);
+        LayoutInflater mInflater = LayoutInflater.from(this);
+
+        View mCustomView = mInflater.inflate(R.layout.actionbar, null);
+        TextView mTitleTextView = (TextView) mCustomView.findViewById(R.id.titletext);
+        mTitleTextView.setText("ActionBar Title");
+
+        ImageButton imageButton = (ImageButton) mCustomView.findViewById(R.id.backButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Button Clicked!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        mActionBar.setCustomView(mCustomView);
+        mActionBar.setDisplayShowCustomEnabled(true);
     }
 
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.actionbar, menu);
-
-        return true;
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
