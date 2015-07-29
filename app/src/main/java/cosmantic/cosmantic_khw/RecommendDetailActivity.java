b00b559a3c78e0f1f84e3b7f5cc139ace6b5b497
Kim Hyeonwoo <1reds@naver.com>
2015-07-29 21:43:08 +0900

    임시 커밋

--- app/src/main/java/cosmantic/cosmantic_khw/RecommendDetailActivity.java
+++ app/src/main/java/cosmantic/cosmantic_khw/RecommendDetailActivity.java
@@ -15,28 +15,18 @@ public class RecommendDetailActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommenddetail);
 
-        ActionBar mActionBar = getActionBar();
-        mActionBar.setDisplayShowHomeEnabled(false);
-        mActionBar.setDisplayShowTitleEnabled(false);
-        LayoutInflater mInflater = LayoutInflater.from(this);
-
         // 액션바 생성 및 제목 추가
-        View mCustomView = mInflater.inflate(R.layout.actionbar, null);
-        TextView mTitleTextView = (TextView) mCustomView.findViewById(R.id.titletext);
+        TextView mTitleTextView = (TextView) View.findViewById(R.id.titletext);
         mTitleTextView.setText("지성용 화장품");
         FontApplyer.setFont(this, mTitleTextView, FontApplyer.Font.NotoSans, FontApplyer.Style.Bold);
 
         // 액션바 뒤로가기 버튼 리스너
-        ImageButton backButton = (ImageButton) mCustomView.findViewById(R.id.backButton);
+        ImageButton backButton = (ImageButton) View.findViewById(R.id.backButton);
         backButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(), "Button Clicked!", Toast.LENGTH_LONG).show();
             }
         });
-
-        // 액션바 출력
-        mActionBar.setCustomView(mCustomView);
-        mActionBar.setDisplayShowCustomEnabled(true);
     }
 
     protected void onDestroy() {
