b17f2df57e5f263218aeae145af94ddddb02436d
sejinq <thstpwlsq@gmail.com>
2015-08-11 18:18:12 +0900

    reviewwirte xml warning 메세지 1차 처리, actionbar1.xml warning 메세지 처리. WriteReviewActivity 리스너 부착, 액티비티 액션바 설정.

--- app/src/main/java/cosmantic/cosmantic_khw/WriteReviewActivity.java
+++ app/src/main/java/cosmantic/cosmantic_khw/WriteReviewActivity.java
@@ -8,6 +8,9 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.widget.Toast.makeText;
 
 public class WriteReviewActivity extends Activity {
 
@@ -24,89 +27,77 @@ public class WriteReviewActivity extends Activity {
     EditText review;
 
     @Override
-    protected void onCreate(Bundle savedInstance){
+    protected void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
         setContentView(R.layout.activity_reviewwrite);
 
-        title= (TextView)findViewById(R.id.titletext);
-        btShare= (ImageButton)findViewById(R.id.sideButton);
+        title = (TextView) findViewById(R.id.titletext);
+
+        image = (ImageView) findViewById(R.id.imageView1);
+        imageShade = (ImageView) findViewById(R.id.imageView2);
+        product = (TextView) findViewById(R.id.textView);
+        brand = (TextView) findViewById(R.id.textView2);
+        average = (TextView) findViewById(R.id.average);
+        myave = (TextView) findViewById(R.id.textScore);
+        review = (EditText) findViewById(R.id.editText);
+
+        btShare = (ImageButton) findViewById(R.id.sideButton);
         btShare.setOnClickListener(new onbtShare());
-
-        image = (ImageView)findViewById(R.id.imageView1);
-        imageShade = (ImageView)findViewById(R.id.imageView2);
-        product= (TextView)findViewById(R.id.textView);
-        brand= (TextView)findViewById(R.id.textView2);
-        average= (TextView)findViewById(R.id.average);
-        myave= (TextView)findViewById(R.id.textScore);
-        btLike= (ImageButton)findViewById(R.id.likebutton);
-        btLike.setOnClickListener(new onbtLike());
-        review= (EditText)findViewById(R.id.editText);
-        btSave= (ImageButton)findViewById(R.id.imageButton8);
+        btSave = (ImageButton) findViewById(R.id.imageButton8);
         btSave.setOnClickListener(new onbtSave());
+        btLike = (ImageButton) findViewById(R.id.likebutton);
+        btLike.setOnClickListener(new onbtLike());
 
-        star[0]= (ImageView)findViewById(R.id.star1);
-        star[1]= (ImageView)findViewById(R.id.star2);
-        star[2]= (ImageView)findViewById(R.id.star3);
-        star[3]= (ImageView)findViewById(R.id.star4);
-        star[4]= (ImageView)findViewById(R.id.star5);
+        star[0] = (ImageView) findViewById(R.id.star1);
+        star[1] = (ImageView) findViewById(R.id.star2);
+        star[2] = (ImageView) findViewById(R.id.star3);
+        star[3] = (ImageView) findViewById(R.id.star4);
+        star[4] = (ImageView) findViewById(R.id.star5);
 
-        btStar[0]= (ImageButton)findViewById(R.id.mystar1);
-        btStar[0].setOnClickListener(new onmystar1());
-        btStar[1]= (ImageButton)findViewById(R.id.mystar2);
-        btStar[1].setOnClickListener(new onmystar2());
-        btStar[2]= (ImageButton)findViewById(R.id.mystar3);
-        btStar[2].setOnClickListener(new onmystar3());
-        btStar[3]= (ImageButton)findViewById(R.id.mystar4);
-        btStar[3].setOnClickListener(new onmystar4());
-        btStar[4]= (ImageButton)findViewById(R.id.mystar5);
-        btStar[4].setOnClickListener(new onmystar5());
+        btStar[0] = (ImageButton) findViewById(R.id.mystar1);
+        btStar[0].setOnClickListener(listener);
+        btStar[1] = (ImageButton) findViewById(R.id.mystar2);
+        btStar[1].setOnClickListener(listener);
+        btStar[2] = (ImageButton) findViewById(R.id.mystar3);
+        btStar[2].setOnClickListener(listener);
+        btStar[3] = (ImageButton) findViewById(R.id.mystar4);
+        btStar[3].setOnClickListener(listener);
+        btStar[4] = (ImageButton) findViewById(R.id.mystar5);
+        btStar[4].setOnClickListener(listener);
 
         title.setText("평가하기");
         btShare.setImageResource(R.drawable.shareButton);
+    }
+    View.OnClickListener listener = new View.OnClickListener() {
+        public void onClick(View v)
+        {
+            switch(v.getId())
+            {
+                case R.id.mystar1: break;
+                case R.id.mystar2:break;
+                case R.id.mystar3:break;
+                case R.id.mystar4:break;
+                case R.id.mystar5:break;
+            }
+        }
+    };
+
+
+    public class onbtShare implements View.OnClickListener {
+        public void onClick(View v) {
+        }
 
     }
+    public class onbtLike implements View.OnClickListener {
+        public void onClick(View v) {
+        }
 
-public class onbtShare implements View.OnClickListener {
-    public void onClick(View v) {
     }
+    public class onbtSave implements View.OnClickListener {
+        public void onClick(View v) {
+        }
 
-}
-public class onbtLike implements View.OnClickListener {
-    public void onClick(View v) {
     }
-
-}
-public class onmystar1 implements View.OnClickListener {
-    public void onClick(View v) {
-    }
-
-}
-public class onmystar2 implements View.OnClickListener {
-    public void onClick(View v) {
-    }
-
-}
-public class onmystar3 implements View.OnClickListener {
-    public void onClick(View v) {
-    }
-
-}
-public class onmystar4 implements View.OnClickListener {
-    public void onClick(View v) {
-    }
-
-}
-public class onmystar5 implements View.OnClickListener {
-    public void onClick(View v) {
-    }
-
-}
-
-public class onbtSave implements View.OnClickListener {
-    public void onClick(View v) {
-    }
-
-}
     @Override
     protected void onDestroy(){
         super.onDestroy();
