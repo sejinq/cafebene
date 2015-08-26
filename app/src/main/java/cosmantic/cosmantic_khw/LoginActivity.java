7bcecf5bc3d7940cbab46d16316c8eb960dea59e
sejinq <thstpwlsq@gmail.com>
2015-08-27 00:07:46 +0900

    8월 26일 수정.진짜끝 ㅠㅠㅠㅠ

--- app/src/main/java/cosmantic/cosmantic_khw/LoginActivity.java
+++ app/src/main/java/cosmantic/cosmantic_khw/LoginActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -70,7 +71,7 @@ public class LoginActivity extends Activity {
                 emailJoinAction();
             }
         });
-        ((ImageButton) findViewById(R.id.login_free)).setOnClickListener(new View.OnClickListener() {
+        ((Button) findViewById(R.id.login_free)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 freeLogin();
